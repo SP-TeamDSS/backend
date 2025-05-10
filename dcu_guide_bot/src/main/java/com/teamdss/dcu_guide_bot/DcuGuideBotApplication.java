a3e5999174a5df8c5d7ab3de576c071bb1a439a9
grovince <grovinceln@gmail.com>
2025-05-11 01:43:06 +0900

    [Init]: 기본 환경 세팅

---
+++ dcu_guide_bot/src/main/java/com/teamdss/dcu_guide_bot/DcuGuideBotApplication.java
@@ -0,0 +1,13 @@
+package com.teamdss.dcu_guide_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcuGuideBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcuGuideBotApplication.class, args);
+	}
+
+}
---
+++ dcu_guide_bot/src/test/java/com/teamdss/dcu_guide_bot/DcuGuideBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamdss.dcu_guide_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcuGuideBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
